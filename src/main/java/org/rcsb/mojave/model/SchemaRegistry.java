3afc4e34d5cc37efab5dc8760b83457c7960c3a5
bioinsilico <jsegurmor@gmail.com>
2024-01-24 09:38:02 -0800

    sequence coordinates classes

--- src/main/java/org/rcsb/mojave/model/SchemaRegistry.java
+++ src/main/java/org/rcsb/mojave/model/SchemaRegistry.java
@@ -99,7 +99,11 @@ public enum SchemaRegistry {
     ANNOTATIONS_POLYMER_ENTITY_INSTANCE_DESCRIPTOR_BSON("validation.annotations.polymer.entity.instance.bson.schema.name"),
 
     ANNOTATIONS_INTERFACE_POLYMER_ENTITY_INSTANCE_DESCRIPTOR_JSON("annotations.interface.polymer.entity.instance.json.schema.name"),
-    ANNOTATIONS_INTERFACE_POLYMER_ENTITY_INSTANCE_DESCRIPTOR_BSON("validation.annotations.interface.polymer.entity.instance.bson.schema.name");
+    ANNOTATIONS_INTERFACE_POLYMER_ENTITY_INSTANCE_DESCRIPTOR_BSON("validation.annotations.interface.polymer.entity.instance.bson.schema.name"),
+    SEQUENCE_COORDINATES_ALIGNMENTS_JSON("sequence.coordinates.alignments.json.schema.name"),
+    SEQUENCE_COORDINATES_ALIGNMENTS_BSON("validation.sequence.coordinates.alignments.bson.schema.name"),
+    SEQUENCE_COORDINATES_SEQUENCES_JSON("sequence.coordinates.sequences.json.schema.name"),
+    SEQUENCE_COORDINATES_SEQUENCES_BSON("validation.sequence.coordinates.sequences.bson.schema.name");
 
     private final String value;
     private static final Map<String, SchemaRegistry> CONSTANTS = new HashMap<>();
